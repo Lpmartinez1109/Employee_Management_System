60299b71a30cd8754d84f428a5b4fc3fa5b05915
LPMartinez <lpmartinez1109@gmail.com>
2020-01-26 09:48:59 -0600

    Created functions to view department, role and employees. need to be able to call the data from sql

--- Master/db/index.js
+++ Master/db/index.js
@@ -1,20 +1,25 @@
 var inquirer = require ("inquirer");
-// var mysql = require("mysql");
+var mysql = require("mysql");
 
-// connection = mysql.createConnection({
-//     host: "local host",
-//     port: 3306,
-//     user: "root",
-//     password: "root",
-//     database: "employee_db"
-// });
+var PORT = process.env.PORT || 8080;
 
-// connection.connect(function(err){
-//     if (err) throw err;
-//     manageMenu();
-// })
+connection = mysql.createConnection({
+    host: "local host",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employee_db"
+});
 
- function manageMenu (){
+connection.connect(function(err){
+    if (err){
+        console.error("error connecting: " + err.stack);
+    }
+    console.log("connect as id " + connection.threadId);
+    manageMenu();
+})
+
+function manageMenu (){
      inquirer.prompt(
          {
              name: "option",
@@ -22,25 +27,50 @@ var inquirer = require ("inquirer");
              message: "What would you like to do?",
              choices: ["View Current Employees", "Add Employees", "Remove Employee", "Update Employee Records", "Exit"] 
          }
-     ).then(function (option){
-         switch (option) {
-             case "View Current Employees":
-                 currentEmployees();
-                 break;
-            case "Add Employees":
-                 addEmployees();
-                 break;
-            case "Remove Employees":
-                 removeEmployees();
-                 break;
-        case "Update Employees Records":
-                 updateEmployees();
-                 break;
-             default:
-                 connection.end()
-                 break;
+     ).then(function(answer){
+         if(answer.option === "View Current Employees"){
+             currentEmployees();
+         }else if(answer.option === "Add Employees"){
+             addEmployees();
+         }else if(answer.option === "Remove Employee"){
+             removeEmployee();
+         }else if(answer.option === "Update Employee Records"){
+           updateEmployees();  
+         }else{
+             connection.end();
          }
      })
  }
+function currentEmployees() {
+    inquirer.prompt(
+        {
+            name: "view",
+            type: "list",
+            message: "What would you like to see?",
+            choices: [ "Department", "Role", "Employees"]
 
- manageMenu();
+        }
+    ).then(function(answer){
+        if(answer.view === "Department"){
+            viewDepartment();
+        }else if(answer.view === "Role"){
+            viewRole();
+        }else if(answer.view === "Employees"){
+            viewEmployee();
+        }
+    })
+};
+    function viewDepartment(){
+        // connection.query("select * from department(name)"  , function(err, result){
+        //     if (err) throw err;
+        //     console.log(result);
+            
+        // })
+        manageMenu();
+    };
+    function viewRole(){
+        manageMenu();
+    }
+    function viewEmployee() {
+        manageMenu();
+    }
