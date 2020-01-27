67bc7c50b6ae08a4ad1e657df4c12b7095670c44
LPMartinez <lpmartinez1109@gmail.com>
2020-01-26 22:15:55 -0600

    made changes to add employee

--- Master/db/index.js
+++ Master/db/index.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-var inquirer = require ("inquirer");
+var inquirer = require("inquirer");
 
 connection = mysql.createConnection({
     host: "localhost",
@@ -9,36 +9,36 @@ connection = mysql.createConnection({
     database: "employee_db"
 });
 
-connection.connect(function(err){
-    if (err){
+connection.connect(function (err) {
+    if (err) {
         console.error("error connecting: " + err.stack);
     }
     console.log("connect as id " + connection.threadId);
     manageMenu();
 })
 
-function manageMenu (){
-     inquirer.prompt(
-         {
-             name: "option",
-             type: "list",
-             message: "What would you like to do?",
-             choices: ["View Current Employees", "Add Employees", "Remove Employee", "Update Employee Records", "Exit"] 
-         }
-     ).then(function(answer){
-         if(answer.option === "View Current Employees"){
-             currentEmployees();
-         }else if(answer.option === "Add Employees"){
-             addEmployees();
-         }else if(answer.option === "Remove Employee"){
-             removeEmployee();
-         }else if(answer.option === "Update Employee Records"){
-           updateEmployees();  
-         }else{
-             connection.end();
-         }
-     })
- }
+function manageMenu() {
+    inquirer.prompt(
+        {
+            name: "option",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View Current Employees", "Add Employees", "Remove Employee", "Update Employee Records", "Exit"]
+        }
+    ).then(function (answer) {
+        if (answer.option === "View Current Employees") {
+            currentEmployees();
+        } else if (answer.option === "Add Employees") {
+            addEmployees();
+        } else if (answer.option === "Remove Employee") {
+            removeEmployee();
+        } else if (answer.option === "Update Employee Records") {
+            updateEmployees();
+        } else {
+            connection.end();
+        }
+    })
+}
 //  view current employees
 function currentEmployees() {
     inquirer.prompt(
@@ -46,81 +46,88 @@ function currentEmployees() {
             name: "view",
             type: "list",
             message: "What would you like to see?",
-            choices: [ "Department", "Role", "Employees"]
+            choices: ["Department", "Role", "Employees", "Go Back"]
 
         }
-    ).then(function(answer){
-        if(answer.view === "Department"){
+    ).then(function (answer) {
+        if (answer.view === "Department") {
             viewDepartment();
-        }else if(answer.view === "Role"){
+        } else if (answer.view === "Role") {
             viewRole();
-        }else if(answer.view === "Employees"){
+        } else if (answer.view === "Employees") {
             viewEmployee();
+        }
+        else {
+            manageMenu();
         }
     })
 };
-    function viewDepartment(){
-        connection.query(`select * from department` , function(err, result){
-            if (err){
-                console.log("Error!") 
-            }
-            console.table(result)
-            manageMenu();
-        })
-    };
-    function viewRole(){
-        connection.query(`select * from role`, function(err,result){
-            if (err){
-                console.log("Error!");
-                
-            }
-            console.table(result)
-            manageMenu();
-        })
+function viewDepartment() {
+    connection.query(`select * from department`, function (err, result) {
+        if (err) {
+            console.log("Error!")
+        }
+        console.table(result)
+        currentEmployees();
+    })
+};
+function viewRole() {
+    connection.query(`select * from role`, function (err, result) {
+        if (err) {
+            console.log("Error!");
 
-    }
-    function viewEmployee() {
-        connection.query(`select * from employee`, function(err, results) {
-            if (err){
-                console.log("Error!");
-                
-            }
-            console.table(results)
-            manageMenu();
-        })
-    }
+        }
+        console.table(result)
+        currentEmployees();
+    })
+
+}
+function viewEmployee() {
+    connection.query(`select * from employee`, function (err, results) {
+        if (err) {
+            console.log("Error!");
+
+        }
+        console.table(results)
+        currentEmployees();
+    })
+}
 // add an employee
 function addEmployees() {
 
-    inquirer.prompt(
+    inquirer.prompt([
         {
-            name: "addFirstName",
+            name: "firstName",
             type: "input",
             message: "What is your employee's first name?"
         },
         {
-           name: "addLastName",
-           type: "input",
-           message: "What is your employee's last name?" 
+            name: "lastName",
+            type: "input",
+            message: "What is your employee's last name?"
         },
         {
-            name: "addRole",
+            name: "employeeRole",
             type: "list",
-            message: "What is this employee's role?",
+            message: "What role does this employee have?",
+            choices: [1, 2, 3, 4]
+
         }
-    ).then(function(answer) {
-        connection.query("insert into employee set ?",
-        {
-            first_name: answer.addFirstName,
-            last_name: answer.addLastName,
-        },
-        function(err) {
-            if (err){
-                console.log("Employee Not Added");
+    ]).then(function (answer) {
+        connection.query("insert into employee(first_name, last_name, role_id", {
+            first_name: answer.firstName,
+            last_name: answer.lastName,
+            role_id: answer.employeeRole
+        }, function (err) {
+            if (err) {
+                console.log("Error!");
+
             }
             manageMenu();
-        }
-        )
-        
-    })
+
+        })
+     })
+}
+// remove employee function
+function removeEmployee() {
 }
